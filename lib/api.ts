fb42c71a718a8faea6af87745b9d4b148fa1c5d9
Dimonik7772 <dimonik7772@gmail.com>
2026-05-02 13:50:45 +0200

    fix api

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Note, NoteFormValues } from "@/types/note";
 
-axios.defaults.baseURL = "https://notehub-public.goit.study/api";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 axios.defaults.headers.common["Authorization"] =
    `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`;
 
